c8bc7fab4964c7ad87cadb8bb51bef4c8cc8e389
Alexey Vedmedev <al.vedmedev@gmail.com>
2022-09-25 14:57:16 +0300

    Update module01_hw1.py

--- module01_hw1.py
+++ module01_hw1.py
@@ -1,9 +1,9 @@
-from abc import ABCMeta, abstractmethod
+from abc import ABC, abstractmethod
 import pickle
 import json
 
 
-class SerializationInterface(metaclass=ABCMeta):
+class SerializationInterface(ABC):
     @abstractmethod
     def read(self, filename):
         ...
